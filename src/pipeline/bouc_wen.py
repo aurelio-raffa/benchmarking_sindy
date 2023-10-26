97b3523fa891fdd26599373a88bbff03e743583b
Aurelio <aurelio.raffa@mail.polimi.it>
2023-10-26 10:00:15 +0200

    refactoring to include HPO on both SINDy models

--- src/pipeline/bouc_wen.py
+++ src/pipeline/bouc_wen.py
@@ -51,18 +51,13 @@ def xdot_model(include_z: bool = True, degree: int = 3, threshold: float = 0.000
     """
     if include_z:
         feature_names = ['x', 'y', 'z', 'u']
-        function_names = functions = [lambda x: x]
-        lib = ps.CustomLibrary(
-            library_functions=functions,
-            function_names=function_names,
-            include_bias=False
-        )
     else:
         feature_names = ['x', 'y', 'u']
-        lib = ps.PolynomialLibrary(
-            include_bias=False,
-            degree=degree,
-        )
+
+    lib = ps.PolynomialLibrary(
+        include_bias=False,
+        degree=degree,
+    )
     optimizer = ps.STLSQ(threshold=threshold, alpha=alpha)
     model = ps.SINDy(
         optimizer=optimizer,
@@ -73,7 +68,7 @@ def xdot_model(include_z: bool = True, degree: int = 3, threshold: float = 0.000
     return model, feature_names
 
 
-def zdot_model(nu: float = 1.0, z_term: bool = True, threshold: float = 1.0):
+def zdot_model(nu: float = 1.0, z_term: bool = True, threshold: float = 1.0, alpha: float = 10.0):
     """library consisting of transformations akin the Bouc-Wen model
     """
     feature_names = ['z', 'x']
@@ -108,7 +103,7 @@ def zdot_model(nu: float = 1.0, z_term: bool = True, threshold: float = 1.0):
         function_names=function_names,
         include_bias=False
     )
-    optimizer = ps.STLSQ(threshold=threshold)
+    optimizer = ps.STLSQ(threshold=threshold, alpha=alpha)
     model = ps.SINDy(
         optimizer=optimizer,
         feature_library=lib,
@@ -116,6 +111,44 @@ def zdot_model(nu: float = 1.0, z_term: bool = True, threshold: float = 1.0):
         differentiation_method=None,
     )
     return model, feature_names
+
+
+def fit_x_model(
+        t_t: np.ndarray,
+        y_t: np.ndarray,
+        x_t: np.ndarray,
+        u_t: np.ndarray,
+        xd_t: np.ndarray,
+        t_v: np.ndarray,
+        y_v: np.ndarray,
+        x_v: np.ndarray,
+        u_v: np.ndarray,
+        xd_v: np.ndarray,
+        verbose: bool = True,
+        threshold: float = 0.00001,
+        alpha: float = 10.0,
+):
+    m0, _ = xdot_model(include_z=False, threshold=threshold, alpha=alpha)
+    m0.fit(np.concatenate([x_t, y_t], axis=1), u=u_t, x_dot=np.concatenate([xd_t, x_t], axis=1))
+
+    try:
+        xy_sindy = m0.simulate(np.array([x_v[0, 0], y_v[0, 0]]), t_v, u=u_v)
+        x_sindy = np.pad(xy_sindy[:, 0], (0, 1), mode='edge').reshape(-1, 1)
+        y_sindy = np.pad(xy_sindy[:, 1], (0, 1), mode='edge').reshape(-1, 1)
+        sindy_r2 = r2_score(y_v.ravel(), y_sindy.ravel())
+        sindy_rmse = np.sqrt(np.mean((y_v.ravel() - y_sindy.ravel()) ** 2))
+
+        if verbose:
+            print('=' * 120)
+            print(f'SINDy naive model:')
+            m0.print()
+            print('-' * 120)
+            print(f'\tR2: {100 * sindy_r2:.3f}%, RMSE: {sindy_rmse:.3e}')
+
+        return m0, x_sindy, y_sindy, sindy_rmse, sindy_r2
+    except Exception:
+        warnings.warn('SINDy naive model failed, terminating prematurely...')
+        return None, None, None, np.inf, -np.inf
 
 
 def fit_z_model(
@@ -137,6 +170,9 @@ def fit_z_model(
         tvr_gamma: float = 0.000001,
         dt: float = 1 / 750.0,
         verbose: bool = True,
+        x_threshold: float = 1e-6,
+        z_threshold: float = 1.0,
+        z_term: bool = True,
 ):
     # We use the first model equation to recover the hidden state z from the initial guess
     # xd = (u - k * y - c * x - z) / m
@@ -151,7 +187,7 @@ def fit_z_model(
 
         # First stage: we use the z estimate to fit a dynamical model based on perfect knowledge of x_dot
         zd = jerk(z_.ravel(), dt, params=[tvr_gamma])[1].reshape(-1, 1)
-        m1_, _ = zdot_model(nu=nu)
+        m1_, _ = zdot_model(nu=nu, z_term=z_term, threshold=z_threshold)
         m1_.fit(z_, u=x_t, x_dot=zd)
 
         try:
@@ -159,7 +195,7 @@ def fit_z_model(
             # by fitting a model for x_dot with z as the simulation of m1, given perfect knowledge of x_dot
             zs = m1_.simulate(np.array([z_[0, 0]]), t_t, u=x_t)
             zs = np.pad(zs.ravel(), (0, 1), mode='edge').reshape(-1, 1)
-            m2_, _ = xdot_model()
+            m2_, _ = xdot_model(degree=1, threshold=x_threshold)
             m2_.fit(x_t, u=np.concatenate([y_t, zs, u_t], axis=1), x_dot=xd_t)
             m2_coef_ = m2_.coefficients().copy()
 
@@ -180,7 +216,7 @@ def fit_z_model(
             z_mod_rmse_ = np.sqrt(np.mean((y_v.ravel() - y_full_.ravel()) ** 2))
 
             if verbose:
-                print('SINDy z model:')
+                print('SINDy hidden model:')
                 m1_.print()
                 m2_.print()
                 print("(y)' = 1.000 x")
@@ -208,17 +244,26 @@ def bouc_wen(
         output_path: str = None,
         dt: float = 1 / 750.0,
         tvr_gamma: float = 0.000001,
-        threshold: float = 0.00001,
-        thresholder: str = "l1",
-        tol: float = 1e-6,
-        max_iter: int = 50000,
         training_samples: int = 1000,
-        validation_samples: int = 1000,
-        alternating_iterations: int = 4,
+        validation_samples: int = 500,
+        model_selection_trials: int = 10,
+        alternating_iterations: int = 1,
+        m_lb: float = 0.2,
+        m_ub: float = 20.0,
+        c_lb: float = 1.0,
+        c_ub: float = 100.0,
+        k_lb: float = 1e4,
+        k_ub: float = 1e6,
+        x_threshold_lb: float = 1e-4,
+        x_threshold_ub: float = 1.0,
+        z_threshold_lb: float = 1e-2,
+        z_threshold_ub: float = 1e2,
+        alpha_lb: float = 1e-3,
+        alpha_ub: float = 1e2,
         verbose: bool = True,
         seed: int = 42,
 ):
-    """Compares a naive SINDy model with a more sophisticated approach on the Bouc-Wen hysteresis benchmark.
+    """Compares a SINDy naive model with a more sophisticated approach on the Bouc-Wen hysteresis benchmark.
     """
     # 01 - Loading the data
     training_data, validation_data, test1_data, test2_data = load_data(
@@ -230,28 +275,48 @@ def bouc_wen(
     t_t = np.arange(0, y_t.shape[0]) * dt
     t_v = np.arange(0, y_v.shape[0]) * dt
 
-    # TODO: HPO also on this model (esp. threshold)
-    m0, _ = xdot_model(include_z=False)
-    m0.fit(np.concatenate([x_t, y_t], axis=1), u=u_t, x_dot=np.concatenate([xd_t, x_t], axis=1))
+    # 02 - Naive SINDy model
+    def x_model_validation_rmse(params: dict):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            val_rmse = fit_x_model(
+                t_t, y_t, x_t, u_t, xd_t,
+                t_v, y_v, x_v, u_v, xd_v,
+                verbose=False,
+                **params
+            )[3]
 
-    xy_sindy = m0.simulate(np.array([x_v[0, 0], y_v[0, 0]]), t_t, u=u_v)
-    x_sindy = np.pad(xy_sindy[:, 0], (0, 1), mode='edge').reshape(-1, 1)
-    y_sindy = np.pad(xy_sindy[:, 1], (0, 1), mode='edge').reshape(-1, 1)
-    sindy_r2 = r2_score(y_v.ravel(), y_sindy.ravel())
-    sindy_rmse = np.sqrt(np.mean((y_v.ravel() - y_sindy.ravel()) ** 2))
+        return val_rmse
+
+    rstate = np.random.Generator(np.random.PCG64(seed=seed))
+    x_model_search_space = {
+        'threshold': hp.loguniform('x_threshold', np.log(x_threshold_lb), np.log(x_threshold_ub)),
+        'alpha': hp.loguniform('alpha', np.log(alpha_lb), np.log(alpha_ub)),
+    }
 
     if verbose:
-        print('=' * 120)
-        print(f'SINDy base model:')
-        m0.print()
-        print('-' * 120)
-        print(f'\tR2: {100 * sindy_r2:.3f}%, RMSE: {sindy_rmse:.3e}')
+        print(' Tuning SINDy naive model '.center(120, '='))
+    x_model_best_parameters = hyperopt.space_eval(
+        x_model_search_space,
+        fmin(
+            fn=x_model_validation_rmse,
+            space=x_model_search_space,
+            algo=tpe.suggest,
+            max_evals=model_selection_trials,
+            rstate=rstate
+        )
+    )
 
-    # fitting the model for the hidden state
-    # TODO: use HPO to tune parameters
-    # initial parameters and hidden state guess
+    m0, x_sindy, y_sindy, sindy_rmse, sindy_r2 = fit_x_model(
+        t_t, y_t, x_t, u_t, xd_t,
+        t_v, y_v, x_v, u_v, xd_v,
+        verbose=verbose,
+        **x_model_best_parameters
+    )
 
-    def validation_rmse(params: dict):
+    # 03 - Fitting the SINDy model for the hidden state
+    def z_model_validation_rmse(params: dict):
+        print(f'Parameters: {params}')
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             val_rmse = fit_z_model(
@@ -266,61 +331,63 @@ def bouc_wen(
 
         return val_rmse
 
+    # Parameter name    | Lower bound   | Upper bound
+    # ------------------|---------------|-------------
+    # mL                | 0.2 kg        | 20 kg
+    # cL                | 1 N/(m/s)     | 100 N/(m/s)
+    # kL                | 1 x 10^4 N/m  | 1 x 10^6 N/m
+    # ------------------------------------------------
+    # from https://doi.org/10.1016/j.ymssp.2018.04.001
+    # Notice that it is also possible to tune parameter nu, which we do not do here
     rstate = np.random.Generator(np.random.PCG64(seed=seed))
-    search_space = {
-        'm': hp.uniform('m', 1.0, 10.0),
-        'k': hp.uniform('k', 1e4, 1e5),
-        'c': hp.uniform('c', 1.0, 100.0),
-        'nu': hp.choice(
-            'nu_',
-            [
-                hp.uniform('nu', 0.5, 1.5),
-                1.0
-            ]
-        )
+    z_model_search_space = {
+        'm': hp.loguniform('m', np.log(m_lb), np.log(m_ub)),
+        'c': hp.loguniform('c', np.log(c_lb), np.log(c_ub)),
+        'k': hp.loguniform('k', np.log(k_lb), np.log(k_ub)),
+        'z_threshold': hp.loguniform('z_threshold', np.log(z_threshold_lb), np.log(z_threshold_ub))
     }
-    best_parameters = hyperopt.space_eval(
-        search_space,
+    
+    if verbose:
+        print(' Tuning SINDy hidden model '.center(120, '='))
+    z_model_best_parameters = hyperopt.space_eval(
+        z_model_search_space,
         fmin(
-            fn=validation_rmse,
-            space=search_space,
+            fn=z_model_validation_rmse,
+            space=z_model_search_space,
             algo=tpe.suggest,
-            max_evals=5,
+            max_evals=model_selection_trials,
             rstate=rstate
         )
     )
 
-    m1, m2, m2_coef, x_full, y_full, z_full, z_mod_rmse, z_mod_r2, z, z_est = fit_z_model(
+    m1, m2, m2_coef, x_z_mod, y_z_mod, z_z_mod, z_mod_rmse, z_mod_r2, z, z_est = fit_z_model(
         t_t, y_t, x_t, u_t, xd_t,
         t_v, y_v, x_v, u_v, xd_v,
         alternating_iterations=alternating_iterations,
         tvr_gamma=tvr_gamma,
         dt=dt,
         verbose=verbose,
-        **best_parameters
+        **z_model_best_parameters
     )
 
+    # 04 - Comparison
     # TODO: run model on test data and save metrics!
 
-    _, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+    _, (ax1, ax3) = plt.subplots(2, 1, sharex=True)
 
-    ax1.plot(t_t, z_est, label='z (estimated)')
-    ax1.plot(t_t, z_full, label='z (simulated)')
-    ax1.plot(t_t, z, label='z (c)')
+    ax1.plot(t_v, x_v, label='x', zorder=4)
+    ax1.plot(t_v, x_z_mod, label='x (simulated)', zorder=3)
+    ax1.plot(t_v, x_sindy, label='x (sindy)', zorder=2)
     ax1.legend()
 
-    ax2.plot(t_t, x_v, label='x', zorder=4)
-    ax2.plot(t_t, x_full, label='x (simulated)', zorder=3)
-    ax2.plot(t_t, x_sindy, label='x (sindy)', zorder=2)
-    ax2.legend()
-
-    ax3.plot(t_t, y_v, label='y', zorder=4)
-    ax3.plot(t_t, y_full, label='y (simulated)', zorder=3)
-    ax3.plot(t_t, y_sindy, label='y (sindy)', zorder=2)
+    ax3.plot(t_v, y_v, label='y', zorder=4)
+    ax3.plot(t_v, y_z_mod, label='y (simulated)', zorder=3)
+    ax3.plot(t_v, y_sindy, label='y (sindy)', zorder=2)
     ax3.legend()
 
     plt.show()
 
+    # TODO: load validation data
     # TODO: replicate hysteresis loop
     # hysteresis loop plotting, i.e. restoring force vs. displacement
     # xd_ = (u_ - k * y_ - c * x_ - z_) / m
