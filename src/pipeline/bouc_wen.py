e514e578e20fa5144a30b39762a7f39f38f68564
Aurelio <aurelio.raffa@mail.polimi.it>
2023-10-26 10:08:40 +0200

    refactoring of bouc_wen dependencies

--- src/pipeline/bouc_wen.py
+++ src/pipeline/bouc_wen.py
@@ -1,243 +1,18 @@
-import os
-import logging
 import warnings
-from copy import deepcopy
 import hyperopt
 
-import pysindy as ps
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 from hyperopt import hp, fmin, tpe
-from typing import Tuple, Any
 from fire import Fire
 from pynumdiff.total_variation_regularization import jerk
 from scipy.integrate import solve_ivp
-from sklearn.metrics import r2_score
 
-
-from __init__ import root_path
 from src.utils.etl import prepare_data
 from src.utils.etl.bouc_wen import load_data
-
-
-silence_loggers = [
-    "hyperopt.tpe",
-    "hyperopt.fmin",
-    "hyperopt.pyll.base",
-]
-for logger in silence_loggers:
-    logging.getLogger(logger).setLevel(logging.ERROR)
-
-
-def ode_model(u: np.ndarray, t: np.ndarray, x_grad: Any, z_grad: Any):
-    # preparing the model for simulation
-    def mod(t_, xyz_):
-        [x_, y_, z_] = xyz_
-        u_ = np.interp(t_, t, u.ravel())
-
-        zd_ = z_grad.predict(np.array([[z_]]), u=np.array([[x_]])).ravel()[0]
-        xd_ = x_grad.predict(np.array([[x_]]), u=np.array([[y_, z_, u_]])).ravel()[0]
-        yd_ = x_
-
-        return np.array([xd_, yd_, zd_])
-
-    return mod
-
-
-def xdot_model(include_z: bool = True, degree: int = 3, threshold: float = 0.00001, alpha: float = 10.0):
-    """simple library consisting of a linear combination of inputs and state
-    """
-    if include_z:
-        feature_names = ['x', 'y', 'z', 'u']
-    else:
-        feature_names = ['x', 'y', 'u']
-
-    lib = ps.PolynomialLibrary(
-        include_bias=False,
-        degree=degree,
-    )
-    optimizer = ps.STLSQ(threshold=threshold, alpha=alpha)
-    model = ps.SINDy(
-        optimizer=optimizer,
-        feature_library=lib,
-        feature_names=feature_names,
-        differentiation_method=None,
-    )
-    return model, feature_names
-
-
-def zdot_model(nu: float = 1.0, z_term: bool = True, threshold: float = 1.0, alpha: float = 10.0):
-    """library consisting of transformations akin the Bouc-Wen model
-    """
-    feature_names = ['z', 'x']
-    if nu != 1.0:
-        functions = [
-            (lambda x: x) if z_term else (lambda _, x: x),
-            lambda z, x: np.power(np.abs(z), nu) * np.abs(x) * np.sign(z),
-            lambda z, x: np.power(np.abs(z), nu) * x,
-            lambda z, x: np.power(np.abs(z), nu) * np.abs(x)
-        ]
-        function_names = [
-            (lambda x: x) if z_term else (lambda _, x: x),
-            lambda z, x: f'|{z}|^{nu:.2f} * |{x}| * sign({z})',
-            lambda z, x: f'|{z}|^{nu:.2f} * {x}',
-            lambda z, x: f'|{z}|^{nu:.2f} * |{x}|'
-        ]
-    else:
-        functions = [
-            (lambda x: x) if z_term else (lambda _, x: x),
-            lambda z, x: z * np.abs(x),
-            lambda z, x: np.abs(z) * x,
-            lambda z, x: np.abs(z) * np.abs(x)
-        ]
-        function_names = [
-            (lambda x: x) if z_term else (lambda _, x: x),
-            lambda z, x: f'{z} * |{x}|',
-            lambda z, x: f'|{z}| * {x}',
-            lambda z, x: f'|{z}| * |{x}|'
-        ]
-    lib = ps.CustomLibrary(
-        library_functions=functions,
-        function_names=function_names,
-        include_bias=False
-    )
-    optimizer = ps.STLSQ(threshold=threshold, alpha=alpha)
-    model = ps.SINDy(
-        optimizer=optimizer,
-        feature_library=lib,
-        feature_names=feature_names,
-        differentiation_method=None,
-    )
-    return model, feature_names
-
-
-def fit_x_model(
-        t_t: np.ndarray,
-        y_t: np.ndarray,
-        x_t: np.ndarray,
-        u_t: np.ndarray,
-        xd_t: np.ndarray,
-        t_v: np.ndarray,
-        y_v: np.ndarray,
-        x_v: np.ndarray,
-        u_v: np.ndarray,
-        xd_v: np.ndarray,
-        verbose: bool = True,
-        threshold: float = 0.00001,
-        alpha: float = 10.0,
-):
-    m0, _ = xdot_model(include_z=False, threshold=threshold, alpha=alpha)
-    m0.fit(np.concatenate([x_t, y_t], axis=1), u=u_t, x_dot=np.concatenate([xd_t, x_t], axis=1))
-
-    try:
-        xy_sindy = m0.simulate(np.array([x_v[0, 0], y_v[0, 0]]), t_v, u=u_v)
-        x_sindy = np.pad(xy_sindy[:, 0], (0, 1), mode='edge').reshape(-1, 1)
-        y_sindy = np.pad(xy_sindy[:, 1], (0, 1), mode='edge').reshape(-1, 1)
-        sindy_r2 = r2_score(y_v.ravel(), y_sindy.ravel())
-        sindy_rmse = np.sqrt(np.mean((y_v.ravel() - y_sindy.ravel()) ** 2))
-
-        if verbose:
-            print('=' * 120)
-            print(f'SINDy naive model:')
-            m0.print()
-            print('-' * 120)
-            print(f'\tR2: {100 * sindy_r2:.3f}%, RMSE: {sindy_rmse:.3e}')
-
-        return m0, x_sindy, y_sindy, sindy_rmse, sindy_r2
-    except Exception:
-        warnings.warn('SINDy naive model failed, terminating prematurely...')
-        return None, None, None, np.inf, -np.inf
-
-
-def fit_z_model(
-        t_t: np.ndarray,
-        y_t: np.ndarray,
-        x_t: np.ndarray,
-        u_t: np.ndarray,
-        xd_t: np.ndarray,
-        t_v: np.ndarray,
-        y_v: np.ndarray,
-        x_v: np.ndarray,
-        u_v: np.ndarray,
-        xd_v: np.ndarray,
-        k: float = 5e4,
-        c: float = 10.0,
-        m: float = 2.0,
-        nu: float = 1.0,
-        alternating_iterations: int = 1,
-        tvr_gamma: float = 0.000001,
-        dt: float = 1 / 750.0,
-        verbose: bool = True,
-        x_threshold: float = 1e-6,
-        z_threshold: float = 1.0,
-        z_term: bool = True,
-):
-    # We use the first model equation to recover the hidden state z from the initial guess
-    # xd = (u - k * y - c * x - z) / m
-    z_est = u_t - k * y_t - c * x_t - m * xd_t
-    z_ = z_est.copy()
-    m1, m2, m2_coef = None, None, None
-    x_full, y_full, z_full = None, None, None
-    z_mod_rmse, z_mod_r2, z = np.inf, - np.inf, None
-    for it in range(alternating_iterations):
-        if verbose:
-            print(f' Iteration {it + 1} '.center(120, '='))
-
-        # First stage: we use the z estimate to fit a dynamical model based on perfect knowledge of x_dot
-        zd = jerk(z_.ravel(), dt, params=[tvr_gamma])[1].reshape(-1, 1)
-        m1_, _ = zdot_model(nu=nu, z_term=z_term, threshold=z_threshold)
-        m1_.fit(z_, u=x_t, x_dot=zd)
-
-        try:
-            # We could proceed with the initial z estimate, but since m1 is not perfect we try to compensate
-            # by fitting a model for x_dot with z as the simulation of m1, given perfect knowledge of x_dot
-            zs = m1_.simulate(np.array([z_[0, 0]]), t_t, u=x_t)
-            zs = np.pad(zs.ravel(), (0, 1), mode='edge').reshape(-1, 1)
-            m2_, _ = xdot_model(degree=1, threshold=x_threshold)
-            m2_.fit(x_t, u=np.concatenate([y_t, zs, u_t], axis=1), x_dot=xd_t)
-            m2_coef_ = m2_.coefficients().copy()
-
-            # Using the model for simulation - here we combine the previous two models, reducing the controls
-            # to u exclusively - notice that we simulate on the validation trajectory.
-            # To recover the initialization of the hidden state we use the first model equation again,
-            # with derivatives up to the second order of the output y on the first validation datapoint.
-            z0 = np.dot(np.array([x_v[0, 0], y_v[0, 0], u_v[0, 0]]), m2_coef_[0, (0, 1, 3)]) - xd_v[0, 0]
-            full_model = ode_model(u_v, t_v, x_grad=m2_, z_grad=m1_)
-
-            xyz_qs = solve_ivp(full_model, [0.0, t_v[-1]], [x_v[0, 0], y_v[0, 0], z0], t_eval=t_v)['y']
-            x_full_ = xyz_qs[0, :].reshape(-1, 1)
-            y_full_ = xyz_qs[1, :].reshape(-1, 1)
-            z_full_ = xyz_qs[2, :].reshape(-1, 1)
-
-            # Computing the performance metrics on the validation trajectory
-            z_mod_r2_ = r2_score(y_v.ravel(), y_full_.ravel())
-            z_mod_rmse_ = np.sqrt(np.mean((y_v.ravel() - y_full_.ravel()) ** 2))
-
-            if verbose:
-                print('SINDy hidden model:')
-                m1_.print()
-                m2_.print()
-                print("(y)' = 1.000 x")
-                print('-' * 120)
-                print(f'\tR2: {100 * z_mod_r2_:.3f}%, RMSE: {z_mod_rmse_:.3e}')
-
-            # The process can be iterated; in this case, we need to recompute the z estimate
-            # based on the parameters fitted in m2
-            z_ = np.dot(np.concatenate([x_t, y_t, u_t], axis=1), m2_coef_[:, (0, 1, 3)].T) - xd_t
-
-            # When using the process iteratively, we save the best model in terms of validation RMSE
-            if z_mod_rmse is None or z_mod_rmse_ < z_mod_rmse:
-                m1, m2, m2_coef = deepcopy(m1_), deepcopy(m2_), m2_coef_.copy()
-                x_full, y_full, z_full = x_full_.copy(), y_full_.copy(), z_full_.copy()
-                z_mod_rmse, z_mod_r2, z = z_mod_rmse_, z_mod_r2_, z_.copy()
-
-        except Exception:
-            warnings.warn(f'Iteration {it + 1} failed, terminating prematurely...')
-            break
-
-    return m1, m2, m2_coef, x_full, y_full, z_full, z_mod_rmse, z_mod_r2, z, z_est
+from src.utils.model_selection.bouc_wen import train_and_validate_x_model, train_and_validate_z_model
+from src.utils.simulation.bouc_wen import ode_model
 
 
 def bouc_wen(
@@ -279,7 +54,7 @@ def bouc_wen(
     def x_model_validation_rmse(params: dict):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            val_rmse = fit_x_model(
+            val_rmse = train_and_validate_x_model(
                 t_t, y_t, x_t, u_t, xd_t,
                 t_v, y_v, x_v, u_v, xd_v,
                 verbose=False,
@@ -307,7 +82,7 @@ def bouc_wen(
         )
     )
 
-    m0, x_sindy, y_sindy, sindy_rmse, sindy_r2 = fit_x_model(
+    m0, x_sindy, y_sindy, sindy_rmse, sindy_r2 = train_and_validate_x_model(
         t_t, y_t, x_t, u_t, xd_t,
         t_v, y_v, x_v, u_v, xd_v,
         verbose=verbose,
@@ -316,10 +91,9 @@ def bouc_wen(
 
     # 03 - Fitting the SINDy model for the hidden state
     def z_model_validation_rmse(params: dict):
-        print(f'Parameters: {params}')
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            val_rmse = fit_z_model(
+            val_rmse = train_and_validate_z_model(
                 t_t, y_t, x_t, u_t, xd_t,
                 t_v, y_v, x_v, u_v, xd_v,
                 alternating_iterations=alternating_iterations,
@@ -360,7 +134,7 @@ def bouc_wen(
         )
     )
 
-    m1, m2, m2_coef, x_z_mod, y_z_mod, z_z_mod, z_mod_rmse, z_mod_r2, z, z_est = fit_z_model(
+    m1, m2, m2_coef, x_z_mod, y_z_mod, z_z_mod, z_mod_rmse, z_mod_r2, z, z_est = train_and_validate_z_model(
         t_t, y_t, x_t, u_t, xd_t,
         t_v, y_v, x_v, u_v, xd_v,
         alternating_iterations=alternating_iterations,
