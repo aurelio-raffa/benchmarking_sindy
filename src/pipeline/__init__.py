8c6ae3a63212e66014eb4810f9d3a6c14ad18b55
Aurelio <aurelio.raffa@mail.polimi.it>
2023-10-28 12:38:05 +0200

    refactoring

--- src/pipeline/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# we add to PATH the root folder
-base_path = os.path.dirname(os.path.abspath(__file__))      # path to directory containing the script
-root_path = os.path.dirname(os.path.dirname(base_path))     # path to directory containing sources (src)
-sys.path.append(root_path)
